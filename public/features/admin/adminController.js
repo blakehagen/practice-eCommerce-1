07ecaf92f6c6c92409df98232586374f23db92ef
Blake Hagen <blakehagen@gmail.com>
2015-11-17 16:55:46 -0700

    admin page styles, need to push data

--- public/features/admin/adminController.js
+++ public/features/admin/adminController.js
@@ -1,7 +1,52 @@
-angular.module('ecommerceApp').controller('adminCtrl', function(){
+angular.module('ecommerceApp').controller('adminCtrl', function ($scope, productService) {
+
+
+    // Toggle Admin Action Buttons //
     
-    
-    
-    
-    
+    $scope.newProductRow = false;
+    $scope.editProductRow = false;
+    $scope.deleteProductRow = false;
+    $scope.idRow = false;
+
+    $scope.newButton = function () {
+        $scope.newProductRow = !$scope.newProductRow;
+        $scope.editProductRow = false;
+        $scope.deleteProductRow = false;
+        $scope.idRow = false;
+    }
+
+    $scope.editButton = function () {
+        $scope.editProductRow = !$scope.editProductRow;
+        $scope.newProductRow = false;
+        $scope.deleteProductRow = false;
+        $scope.idRow = false;
+    }
+
+    $scope.deleteButton = function () {
+        $scope.deleteProductRow = !$scope.deleteProductRow;
+        $scope.newProductRow = false;
+        $scope.editProductRow = false;
+        $scope.idRow = false;
+    }
+
+    $scope.productDetailsButton = function () {
+        $scope.idRow = !$scope.idRow;
+        $scope.newProductRow = false;
+        $scope.editProductRow = false;
+        $scope.deleteProductRow = false;
+    }
+
+
+    // Submit Data to Database //
+
+    $scope.productDetails = false;
+
+    $scope.get = function () {
+        productService.getProducts().then(function (response) {
+            $scope.products = response;
+        })
+        $scope.productDetails = !$scope.productDetails;
+    }
+
+
 });
