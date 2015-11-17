8d0c07cdfbf258635ff99673777492464d10acb6
Blake Hagen <blakehagen@gmail.com>
2015-11-17 14:18:56 -0700

    step3 done

--- server.js
+++ server.js
@@ -8,6 +8,41 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+var db = mongojs('ecommerce');
+var products = db.collection('products');
+
+
+app.get('/products', function (req, res, next) {
+    products.find(req.query, function (err, result) {
+        if (err) res.send(err);
+        else res.send(result);
+    })
+});
+
+
+app.post('/products', function (req, res, next) {
+    products.insert(req.body, function (err, result) {
+        if (err) res.send(err);
+        else res.send(result);
+    })
+});
+
+app.put('/products', function (req, res, next) {
+    products.update({ "_id": mongojs.ObjectId(req.query.id) }, req.body, function (err, result) {
+        if (err) res.send(err);
+        else res.send(result);
+    })
+});
+
+
+app.delete('/products', function (req, res, next) {
+    // console.log(req.query.id);
+    products.remove({ "_id": mongojs.ObjectId(req.query.id) }, function (err, result) {
+        if (err) res.send(err);
+        else res.send(result);
+    })
+});
+
 
 
 
